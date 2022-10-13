0a05e5c5af2e9633b0684107043a2cdbd9853e43
Slim BEN AMOR <slim.ben-amor.auditeur@lecnam.net>
2022-10-13 17:23:10 +0000

    Delete non-needed files from replit

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-    deps = [
-        pkgs.gradle_5
-        pkgs.graalvm17-ce
-        pkgs.maven
-        pkgs.replitPackages.jdt-language-server
-        pkgs.replitPackages.java-debug
-    ];
-}
